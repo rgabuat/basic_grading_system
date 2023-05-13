d46d03e1118d9430682b6b85b2d397909ba7bb00
rgabuat <johncarlgabuat@gmail.com>
2023-05-13 20:25:51 +0800

    user_table_migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fname');
+            $table->string('lname');
             $table->string('email')->unique();
+            $table->string('dob');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default('false');
             $table->rememberToken();
             $table->timestamps();
         });
