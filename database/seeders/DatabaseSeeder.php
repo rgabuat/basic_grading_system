65da042ae3b21ce5482a98ec03208fbe382522a7
rgabuat <johncarlgabuat@gmail.com>
2023-05-13 09:03:47 +0800

    grading_period

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             RolesAndPermissionsSeeder::class,
             SemesterSeeder::class,
             YearLevelSeeder::class,
+            GradingPeriodSeeder::class,
         ]);
 
     }
