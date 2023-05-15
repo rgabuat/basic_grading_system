9c778e4780891cd0f9cc13dc64bbf8d383dcbc84
rgabuat <johncarlgabuat@gmail.com>
2023-05-15 10:16:19 +0800

    branding

---
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -0,0 +1,12 @@
+<div>
+  <img
+    src="{{ asset('/images/CICLogo.png') }}"
+    alt="Icon"
+    class="relative z-20 block h-10 w-full object-contain dark:hidden"
+  />
+  <img
+    src="{{ asset('/images/CICLogo.png') }}"
+    alt="Icon"
+    class="relative z-20 hidden h-10 w-full object-contain dark:block"
+  />
+</div>
---
+++ resources/views/vendor/filament/components/footer.blade.php
@@ -0,0 +1,29 @@
+{{ \Filament\Facades\Filament::renderHook('footer.before') }}
+ 
+<div class="filament-footer flex items-center justify-center">
+  {{ \Filament\Facades\Filament::renderHook('footer.start') }}
+ 
+  @if (config('filament.layout.footer.should_show_logo'))
+    <a
+      href="https://filamentphp.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="text-gray-300 transition hover:text-primary-500"
+    >
+      <img
+        src="{{ asset('/images/CICLogo.png') }}"
+        alt=""
+        class="h-10 dark:hidden"
+      >
+      <img
+        src="{{ asset('/images/CICLogo.png') }}"
+        alt=""
+        class="hidden h-10 dark:block"
+      >
+    </a>
+  @endif
+ 
+  {{ \Filament\Facades\Filament::renderHook('footer.end') }}
+</div>
+ 
+{{ \Filament\Facades\Filament::renderHook('footer.after') }}
