e485e9084bffff938cedbde29676f4de765764b8
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2023-05-16 18:21:28 +0800

    add email

---
+++ app/Mail/DemoMail.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\HtmlString;
+
+class DemoMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $mailData;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($mailData)
+    {
+        $this->mailData = $mailData;
+    }
+
+    public function build()
+    {
+        $tableContent = new HtmlString($this->mailData['body']);
+
+        return $this->view('emails.demoMail')
+            ->subject($this->mailData['title'])
+            ->with('tableContent', $tableContent);
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'This is your Sons/Daughters Grades',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.DemoMail',
+        );
+    }
+
+
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
--- resources/views/filament/resources/students-resource/pages/subjects.blade.php
+++ resources/views/filament/resources/students-resource/pages/subjects.blade.php
@@ -1,5 +1,16 @@
 <x-filament::page>
 
+@if (session()->has('message'))
+    <div class="px-4 py-2 mt-4 text-sm font-medium text-white bg-green-500">
+        {{ session('message') }}
+    </div>
+@endif
+    <button wire:click="sendEmail" wire:loading.attr="disabled" class="px-4 py-2 mt-4 text-sm font-medium text-white bg-gray-500 hover:bg-gray-600 rounded-md">
+    <span wire:loading wire:target="sendEmail" class="mr-2">
+        <i class="fa fa-spinner fa-spin"></i>
+    </span>
+    Send Email
+</button>
 <table class="w-full border-collapse border border-gray-400">
     <thead>
         <tr>
@@ -17,37 +28,38 @@
         </tr>
     </thead>
     <tbody>
-        @foreach ($subjects as $grade)
-            <tr class="bg-gray-100 ">
-                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-b">{{$grade->name}}</td>
-                @php
-                    $finalGrade = 0;
-                @endphp
-                @foreach($grade->gradingPeriod as $key => $gradingPeriod)
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-b">
-                        {{$gradingPeriod->total * 100}}
-                        @php
-                            if($gradingPeriod->name == 'Prelim' || $gradingPeriod->name == 'Midterm'){
-                                $percent = 30;
-                            } else {
-                                $percent = 40;
-                            }
-
-                            $finalGrade += ($gradingPeriod->total * 100) * ($percent / 100);
-                        @endphp
-                    </td>
-                @endforeach
-                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-b">{{$finalGrade}}</td>
+       @foreach ($subjects as $grade)
+    <tr class="bg-gray-100 ">
+        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-b">{{$grade->name}}</td>
+        @php
+            $finalGrade = 0;
+        @endphp
+        @if (!empty($grade->gradingPeriod))
+            @foreach($grade->gradingPeriod as $key => $gradingPeriod)
                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-b">
-                    @if($finalGrade > 75 )
-                    Passed
-                    @else
-                    Failed
-                    @endif
-                </td>
+                    {{$gradingPeriod->total * 100}}
+                    @php
+                        if($gradingPeriod->name == 'Prelim' || $gradingPeriod->name == 'Midterm'){
+                            $percent = 30;
+                        } else {
+                            $percent = 40;
+                        }
 
-            </tr>
-        @endforeach
+                        $finalGrade += ($gradingPeriod->total * 100) * ($percent / 100);
+                    @endphp
+                </td>
+            @endforeach
+        @endif
+        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-b">{{$finalGrade}}</td>
+        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-b">
+            @if($finalGrade > 75 )
+                Passed
+            @else
+                Failed
+            @endif
+        </td>
+    </tr>
+@endforeach
 
     </tbody>
 </table>
